2bba58773fee2be74ecc56d6956e05023e1aaee3
Ana Serra <ana.nogueiraserra@icloud.com>
2024-11-19 15:44:28 +0100

    fix conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_132017) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_110650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,4 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_132017) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
+  add_foreign_key "bookings", "activities"
+  add_foreign_key "bookings", "users"
 end
