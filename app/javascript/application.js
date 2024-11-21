78799bf836899c6e384cb84374c75374685414a6
Etienne Verschave <etienneverschave@gmail.com>
2024-11-21 15:48:12 +0100

    Add booking pop-up, + flatpickr

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,26 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("bookingModal");
+  const openBtn = document.getElementById("openModal");
+  const closeBtn = document.querySelector(".close");
+
+  // Ouvrir la modale
+  openBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  // Fermer la modale
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  // Fermer la modale en cliquant à l'extérieur
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
